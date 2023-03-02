46dda277abfc2f9866582c5f16a6d073a595610d
MacBook Air <q1575599749@outlook.com>
2023-03-03 00:53:01 +0800

    add new MySQL

---
+++ src/main/java/com/woniuxy/day022/CentOSDemo.java
@@ -0,0 +1,26 @@
+package com.woniuxy.day022;
+/**
+ * jdbc:mysql://18.162.120.240:3306/ + <database name>
+ */
+
+import java.sql.*;
+
+public class CentOSDemo {
+    public static void main(String[] args) {
+        //加载驱动 Java8可选
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        //注意端口号和database名称
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://18.162.120.240:3306/db01", "root", "123456");
+            PreparedStatement pstmt = connection.prepareStatement("INSERT INTO test(name) VALUES ('Tom')");
+            pstmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
